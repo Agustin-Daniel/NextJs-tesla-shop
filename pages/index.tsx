f97f5940ea58081576d95fdfb9920fd11580b91b
Agustin Daniel Messina <agustindmessina@gmail.com>
2023-03-29 06:01:37 -0300

    Working in ShopLayout.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import { ShopLayout } from "@/components/layout";
+import { Typography } from "@mui/material";
 
 
 export default function Home() {
   return (
-    <h1>Teslo-Shop</h1>
+    <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos de teslo aqui'}>
+      <Typography variant="h1" component='h1'>Tienda</Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>Todos los productos</Typography>
+    </ShopLayout>
   )
 }
